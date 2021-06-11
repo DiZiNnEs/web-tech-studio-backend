689a37b505b13f1f16663cb1e75c6d25e2d5537b
dizinnes <dizinnes228@gmail.com>
2021-06-11 16:22:15 +0600

    feat(main_app/views): Create send email feature and update template

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,35 @@
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import TemplateView
 
+from django.conf import settings
+from django.core.mail import send_mail
 
-class IndexView(TemplateView):
+
+class IndexView(View):
     template_name = 'main_app/index.html'
+
+    def get(self, request):
+        return render(request, template_name=self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        title = request.POST.get('title')
+        desc = request.POST.get('desk')
+
+        subject = 'IT - WebTech studio'
+        message = f'''
+            Здравствуйте, {username}!
+            Благодарю за заявку! В скором времени мы ответим на неё.
+            
+            С уважением,
+            Х.
+        '''
+        email_from = settings.EMAIL_HOST_USER
+        recipients = [email, ]
+        send_mail(subject=subject, message=message, from_email=email_from, recipient_list=recipients)
+        return redirect('/')
 
 
 class ServicesView(TemplateView):
