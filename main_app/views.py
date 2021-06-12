b2e3f6cb0d962c626785fda4ad819ea108db7181
dizinnes <dizinnes228@gmail.com>
2021-06-12 19:16:56 +0600

    feat(main_app/views): Create forms.py and add it to `IndexView`

--- main_app/views.py
+++ main_app/views.py
@@ -1,35 +1,43 @@
-from django.shortcuts import render, redirect
-from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-
+from django.views.generic.edit import CreateView
 from django.conf import settings
 from django.core.mail import send_mail
 
+from main_app.forms import CustomerForm
+from main_app.models import Project
 
-class IndexView(View):
+
+class IndexView(CreateView):
     template_name = 'main_app/index.html'
+    form_class = CustomerForm
+    success_url = reverse_lazy('index')
 
-    def get(self, request):
-        return render(request, template_name=self.template_name)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['six_projects'] = Project.objects.all()[:6]
+        context['our_project'] = Project.objects.all()[:4]
+        return context
 
-    def post(self, request):
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        title = request.POST.get('title')
-        desc = request.POST.get('desk')
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
 
         subject = 'IT - WebTech studio'
         message = f'''
-            Здравствуйте, {username}!
-            Благодарю за заявку! В скором времени мы ответим на неё.
-            
-            С уважением,
-            Х.
-        '''
+                    Здравствуйте, {username}!
+                    Благодарю за заявку! В скором времени мы ответим на неё.
+
+                    С уважением,
+                    Х.
+                '''
         email_from = settings.EMAIL_HOST_USER
         recipients = [email, ]
         send_mail(subject=subject, message=message, from_email=email_from, recipient_list=recipients)
-        return redirect('/')
+
+        super().form_valid(form)
+        return redirect(self.success_url)
 
 
 class ServicesView(TemplateView):
