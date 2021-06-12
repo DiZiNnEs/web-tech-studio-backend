ca0333277fb8e63e9fea0965a59fa6a130ff81b3
dizinnes <dizinnes228@gmail.com>
2021-06-12 16:39:02 +0600

    refactor(main_app/media): Change `media` upload path and add `__str__` method

--- main_app/models.py
+++ main_app/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=32, verbose_name='Название проекта')
     count = models.SmallIntegerField(verbose_name='количество выполненых проектов')
-    image = models.ImageField(upload_to='images/projects/%Y/%m/')
+    image = models.ImageField(upload_to='media/images/projects/%Y/%m/')
 
     class Meta:
         db_table = 'project'
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.name
